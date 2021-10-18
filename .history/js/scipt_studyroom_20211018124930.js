bcd689fbf2eea294ba1129348ea5b4e700ab420e
runx1ng <runxingli6@gmail.com>
2021-10-18 12:54:46 +1000

    task list changes

--- .history/js/scipt_studyroom_20211018124930.js
+++ .history/js/scipt_studyroom_20211018124930.js
@@ -58,6 +58,17 @@ function taskDisplay() {
     
 }
 
+$(".create_button").click(function(){
+    var message1 = $("#title").val();
+    var message2 = $("#date").val();
+    var message3 = $("#time1").val();
+    var message4 = $("#time2").val();
+
+    document.getElementsByClassName("callout-task")[0].display='none';
+    if(message1 != ""){
+        $("#text").append("<p>"+message1+message2+message3+message4+"</p>");
+    }
+});
 
 // Reference
 // 1. https://www.w3schools.com/js/default.asp
